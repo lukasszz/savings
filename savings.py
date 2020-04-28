365a1afb0474cb5383337efcf3117f61c6082156
lukasszz <lukasz@lukaszherok.com>
2020-04-28 22:29:44 +0200

    ~/.savings

--- savings.py
+++ savings.py
@@ -1,19 +1,30 @@
 import locale
 import sys
+from pathlib import Path
+from shutil import copyfile
 
 from PySide2.QtWidgets import QApplication
 
 import db
 
 
+def first_run():
+    savdir = Path.home() / '.savings'
+    if savdir.exists():
+        return
+    savdir.mkdir()
+    copyfile('db.sqlite', str(savdir / 'db.sqlite'))
+
+
 if __name__ == "__main__":
     locale.setlocale(locale.LC_ALL, '')
     app = QApplication(sys.argv)
 
-    db.setup('sqlite:///db.sqlite')
+    first_run()
+
+    db.setup('sqlite:////' + str(Path.home() / '.savings' / 'db.sqlite'))
     from form.MainWindow import MainWindow
+
     w = MainWindow()
-
     w.show()
-
     sys.exit(app.exec_())
