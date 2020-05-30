4c9ae9b53e907378e6ac9b232b6c35e8bf0e93ae
lukasszz <lukasz@lukaszherok.com>
2020-05-30 08:38:31 +0200

    rpm version 1.0.0

--- build/linux_rpm_build.sh
+++ build/linux_rpm_build.sh
@@ -9,4 +9,4 @@ rm target/savings.rpm
 
 mv dist/savings dist/opt/
 
-fpm -s dir --log error --rpm-rpmbuild-define "_build_id_links none" -C dist -n savings -v 1.0 --vendor lukasszz -t rpm -p target/savings.rpm
+fpm -s dir --log error --rpm-rpmbuild-define "_build_id_links none" -C dist -n savings -v 1.0.0 --vendor lukasszz -t rpm -p target/savings.rpm
